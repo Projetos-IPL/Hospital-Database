abafd6bb67f42a1c0925eeaeb29a44a8ebfd2374
Afonso Santos <afonso.faisca1@gmail.com>
2022-04-12 15:20:52 +0100

    Added package procedures and dummy data

---
+++ dummy_data/create_areas_atuacao.sql
@@ -0,0 +1,25 @@
+INSERT INTO area_atuacao (descricao)
+    VALUES ('Unidade do Ombro e Cotovelo');
+    
+INSERT INTO area_atuacao (descricao)
+    VALUES ('Unidade da Anca e Bacia');
+    
+INSERT INTO area_atuacao (descricao)
+    VALUES ('Unidade da Patologia da Coluna Vertebral');
+    
+INSERT INTO area_atuacao (descricao)
+    VALUES ('Unidade de Punho e Mão');
+    
+INSERT INTO area_atuacao (descricao)
+    VALUES ('Unidade de Ortopedia da Criança e do Adolescente');
+    
+INSERT INTO area_atuacao (descricao)
+    VALUES ('Unidade do Joelho e Tibio-Társica, Artroscopia e Traumatologia Desportiva');
+    
+INSERT INTO area_atuacao (descricao)
+    VALUES ('Unidade do Pé e Tornozelo');
+    
+INSERT INTO area_atuacao (descricao)
+    VALUES ('Unidade de Tumores Ósseos e de Partes Moles');
+
+COMMIT;
--- packages/et_pessoa/et_pessoa_body.sql
+++ packages/et_pessoa/et_pessoa_body.sql
@@ -14,6 +14,7 @@ CREATE OR REPLACE PACKAGE BODY et_pessoa AS
 
     END adicionar_pessoa;
 
+
     PROCEDURE adicionar_paciente(
         p_nif            IN pessoa.nif%TYPE,
         p_prim_nome      IN pessoa.prim_nome%TYPE,
@@ -47,6 +48,92 @@ CREATE OR REPLACE PACKAGE BODY et_pessoa AS
             ROLLBACK;
 
     END adicionar_paciente;
+
+
+    PROCEDURE adicionar_funcionario(p_rec_pessoa IN pessoa%ROWTYPE) IS
+    BEGIN
+        adicionar_pessoa(p_rec_pessoa);
+        
+        SET TRANSACTION READ WRITE NAME 'Adicionar Funcionário';
+        INSERT INTO funcionario VALUES (p_rec_pessoa.nif);
+        COMMIT;
+
+        EXCEPTION
+            WHEN OTHERS THEN
+                dbms_output.PUT_LINE('Error code: '|| SQLCODE);
+                dbms_output.PUT_LINE(SQLERRM);
+                ROLLBACK;
+
+    END adicionar_funcionario;
+    
+    
+    PROCEDURE adicionar_enfermeiro(
+        p_nif            IN pessoa.nif%TYPE,
+        p_prim_nome      IN pessoa.prim_nome%TYPE,
+        p_ult_nome       IN pessoa.ult_nome%TYPE,
+        p_morada         IN pessoa.morada%TYPE,
+        p_telefone       IN pessoa.telefone%TYPE
+    )
+    IS
+        rec_pessoa pessoa%ROWTYPE;
+    BEGIN
+        rec_pessoa.nif := p_nif;
+        rec_pessoa.prim_nome := p_prim_nome;
+        rec_pessoa.ult_nome := p_ult_nome;
+        rec_pessoa.morada := p_morada;
+        rec_pessoa.telefone := p_telefone;
+
+        adicionar_funcionario(rec_pessoa);
+
+        SET TRANSACTION READ WRITE NAME 'Adicionar Enfermeiro';
+
+        INSERT INTO enfermeiro
+            VALUES (p_nif);
+
+        COMMIT;
+
+        EXCEPTION
+            WHEN OTHERS THEN
+                dbms_output.PUT_LINE('Error code: '|| SQLCODE);
+                dbms_output.PUT_LINE(SQLERRM);
+            ROLLBACK;
+
+    END adicionar_enfermeiro;
+    
+    
+    PROCEDURE adicionar_medico(
+        p_nif               IN pessoa.nif%TYPE,
+        p_prim_nome         IN pessoa.prim_nome%TYPE,
+        p_ult_nome          IN pessoa.ult_nome%TYPE,
+        p_morada            IN pessoa.morada%TYPE,
+        p_telefone          IN pessoa.telefone%TYPE,
+        p_id_area_atuacao   IN medico.id_area_atuacao%TYPE
+    )
+    IS
+        rec_pessoa pessoa%ROWTYPE;
+    BEGIN
+        rec_pessoa.nif := p_nif;
+        rec_pessoa.prim_nome := p_prim_nome;
+        rec_pessoa.ult_nome := p_ult_nome;
+        rec_pessoa.morada := p_morada;
+        rec_pessoa.telefone := p_telefone;
+
+        adicionar_funcionario(rec_pessoa);
+
+        SET TRANSACTION READ WRITE NAME 'Adicionar Médico';
+
+        INSERT INTO medico
+            VALUES (p_nif, p_id_area_atuacao);
+
+        COMMIT;
+
+        EXCEPTION
+            WHEN OTHERS THEN
+                dbms_output.PUT_LINE('Error code: '|| SQLCODE);
+                dbms_output.PUT_LINE(SQLERRM);
+            ROLLBACK;
+
+    END adicionar_medico;
 
 END et_pessoa;
 
