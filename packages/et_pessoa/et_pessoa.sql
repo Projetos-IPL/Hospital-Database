833686b7ae9a56f9328bfd0ea0733aae6aa8f656
Afonso Santos <afonso.faisca1@gmail.com>
2022-04-12 15:49:14 +0100

    Added headers

--- packages/et_pessoa/et_pessoa.sql
+++ packages/et_pessoa/et_pessoa.sql
@@ -16,6 +16,23 @@ CREATE OR REPLACE PACKAGE et_pessoa AS
         p_telefone       IN pessoa.telefone%TYPE,
         p_n_utente_saude IN paciente.n_utente_saude%TYPE
     );
+    
+    PROCEDURE adicionar_enfermeiro(
+        p_nif            IN pessoa.nif%TYPE,
+        p_prim_nome      IN pessoa.prim_nome%TYPE,
+        p_ult_nome       IN pessoa.ult_nome%TYPE,
+        p_morada         IN pessoa.morada%TYPE,
+        p_telefone       IN pessoa.telefone%TYPE
+    );
+    
+    PROCEDURE adicionar_medico(
+        p_nif               IN pessoa.nif%TYPE,
+        p_prim_nome         IN pessoa.prim_nome%TYPE,
+        p_ult_nome          IN pessoa.ult_nome%TYPE,
+        p_morada            IN pessoa.morada%TYPE,
+        p_telefone          IN pessoa.telefone%TYPE,
+        p_id_area_atuacao   IN medico.id_area_atuacao%TYPE
+    );
 
 END et_pessoa;
 /
