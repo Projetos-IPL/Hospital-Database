e4708c71f445235f5a58c1c8bb11b2f19b5ad199
Afonso Santos <afonso.faisca1@gmail.com>
2022-04-12 11:13:51 +0100

    Fixed keywords case

--- triggers.sql
+++ triggers.sql
@@ -1,20 +1,8 @@
-/*
-********************************************************
-
-    DEFINIÇÃO DE TRIGGERS
-
-    Afonso Santos, Iúri Raimundo
-    Deloitte Delivery Center S.A - Brightstart
-
-*********************************************************
-*/
-
-
 CREATE OR REPLACE TRIGGER tbi_seq_area_atuacao BEFORE
     INSERT ON area_atuacao
     FOR EACH ROW
 BEGIN
-    :new.id_area_atuacao := pk_area_atuacao_seq.nextval;
+    :NEW.id_area_atuacao := pk_area_atuacao_seq.NEXTVAL;
 END;
 /
 
@@ -22,7 +10,7 @@ CREATE OR REPLACE TRIGGER tbi_seq_cirurgia BEFORE
     INSERT ON cirurgia
     FOR EACH ROW
 BEGIN
-    :new.id_cirurgia := pk_cirurgia_seq.nextval;
+    :NEW.id_cirurgia := pk_cirurgia_seq.NEXTVAL;
 END;
 /
 
@@ -30,7 +18,7 @@ CREATE OR REPLACE TRIGGER tbi_seq_consulta BEFORE
     INSERT ON consulta
     FOR EACH ROW
 BEGIN
-    :new.id_consulta := pk_consulta_seq.nextval;
+    :NEW.id_consulta := pk_consulta_seq.NEXTVAL;
 END;
 /
 
@@ -38,7 +26,7 @@ CREATE OR REPLACE TRIGGER tbi_seq_relatorio BEFORE
     INSERT ON relatorio
     FOR EACH ROW
 BEGIN
-    :new.id_relatorio := pk_relatorio_seq.nextval;
+    :NEW.id_relatorio := pk_relatorio_seq.NEXTVAL;
 END;
 /
 
@@ -46,7 +34,7 @@ CREATE OR REPLACE TRIGGER tbi_seq_tipo_cirurgia BEFORE
     INSERT ON tipo_cirurgia
     FOR EACH ROW
 BEGIN
-    :new.id_tipo_cirurgia := pk_tipo_cirurgia_seq.nextval;
+    :NEW.id_tipo_cirurgia := pk_tipo_cirurgia_seq.NEXTVAL;
 END;
 /
 
@@ -54,6 +42,9 @@ CREATE OR REPLACE TRIGGER tbi_seq_tratamento BEFORE
     INSERT ON tratamento
     FOR EACH ROW
 BEGIN
-    :new.id_tratamento := pk_tratamento_seq.nextval;
+    :NEW.id_tratamento := pk_tratamento_seq.NEXTVAL;
 END;
 /
+
+COMMIT;
+      
