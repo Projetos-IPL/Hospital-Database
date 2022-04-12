2847f6cbaa368f2e137d07b42eaabf8ef338d066
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-04-12 11:13:50 +0100

    Ficheiro para apagar todas as tabelas

---
+++ eliminar.sql
@@ -0,0 +1,62 @@
+/*
+********************************************************
+
+    Eliminação dos objetos da base de dados
+
+    Afonso Santos, Iúri Raimundo
+    Deloitte Delivery Center S.A - Brightstart
+
+*********************************************************
+*/
+
+
+DROP TABLE medico_cirurgia;
+/
+
+DROP TABLE cirurgia;
+/
+
+DROP TABLE consulta;
+/
+
+DROP TABLE tratamento;
+/
+
+DROP TABLE relatorio;
+/
+
+DROP TABLE enfermeiro;
+/
+
+DROP TABLE medico;
+/
+
+DROP TABLE funcionario;
+/
+
+DROP TABLE paciente;
+/
+
+DROP TABLE pessoa;
+/
+
+DROP TABLE estado_paciente;
+/
+
+DROP TABLE tipo_cirurgia;
+/
+
+DROP TABLE area_atuacao;
+/
+
+
+
+
+
+
+
+
+
+
+
+
