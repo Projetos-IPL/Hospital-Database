84fa7904c2da0066e58045558df39e6c8cd61f30
Afonso Santos <afonso.faisca1@gmail.com>
2022-04-13 20:21:24 +0100

    Removed exceptions from packages

--- dummy_data/index.sql
+++ dummy_data/index.sql
@@ -1,5 +1,6 @@
 @@ create_areas_atuacao.sql
 @@ create_estados_pacientes.sql
 @@ create_tipos_cirurgias.sql
+@@ create_user_exceptions.sql
 
 COMMIT;
--- packages/et_relatorio/et_relatorio.sql
+++ packages/et_relatorio/et_relatorio.sql
@@ -1,9 +1,5 @@
 CREATE OR REPLACE PACKAGE et_relatorio AS
 
-    ex_alteracao_relatorio EXCEPTION;
-    ex_alteracao_relatorio_error_code INT := -20699;
-    ex_alteracao_relatorio_errm VARCHAR2(100) := 'Tentativa de alteração de um relatório';
-
     -- Função para adicionar um relatório, devolvendo o ID criado
     FUNCTION adicionar_relatorio(
         p_nif       IN relatorio.nif%TYPE,
