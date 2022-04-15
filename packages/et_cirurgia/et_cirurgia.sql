6ecabcf78329451e13dcf6cb6ace7527ec4f0554
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-04-15 17:25:41 +0100

    commit

--- packages/et_cirurgia/et_cirurgia.sql
+++ packages/et_cirurgia/et_cirurgia.sql
@@ -10,8 +10,8 @@ CREATE OR REPLACE PACKAGE et_cirurgia AS
         p_relatorio             IN relatorio.texto%TYPE,
         p_t_nif                 IN et_pessoa.t_nif);
 
-    /* Função para validar nova cirurgia, devolve TRUE se for válida, devolve FALSE se não for.
-    Uma cirurgia não é válida quando */
+    -- Função para validar nova cirurgia, devolve TRUE se for válida, devolve FALSE se não for.
+    -- Uma cirurgia não é válida quando a área de atuação do tratamento não corresponde à do tipo cirurgia
     FUNCTION validar_cirurgia(
         p_id_tratamento       IN cirurgia.id_tratamento%TYPE,
         p_id_tipo_cirurgia    IN cirurgia.id_tipo_cirurgia%TYPE,
