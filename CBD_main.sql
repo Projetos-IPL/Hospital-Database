f2bd1fae03945dc3c534e04682218ded7953b41c
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-04-15 16:21:06 +0100

    commit

--- CBD_main.sql
+++ CBD_main.sql
@@ -1,26 +1,26 @@
 -- Entry point para criação dos objetos na base de dados
 
-@@ CBD_eliminar.sql
+@ CBD_eliminar.sql
 
-@@ CBD_tabelas.sql
+@ CBD_tabelas.sql
 
-@@ packages\exception_handler\exception_handler.sql
-@@ packages\exception_handler\exception_handler_body.sql
+@ packages\exception_handler\exception_handler.sql
+@ packages\exception_handler\exception_handler_body.sql
 
-@@ packages\et_cirurgia\et_cirurgia.sql
-@@ packages\et_consulta\et_consulta.sql
-@@ packages\et_pessoa\et_pessoa.sql
-@@ packages\et_relatorio\et_relatorio.sql
-@@ packages\et_tratamento\et_tratamento.sql
+@ packages\et_cirurgia\et_cirurgia.sql
+@ packages\et_consulta\et_consulta.sql
+@ packages\et_pessoa\et_pessoa.sql
+@ packages\et_relatorio\et_relatorio.sql
+@ packages\et_tratamento\et_tratamento.sql
 
-@@ CBD_views.sql
+@ CBD_views.sql
 
-@@ packages\et_cirurgia\et_cirurgia_body.sql
-@@ packages\et_consulta\et_consulta_body.sql
-@@ packages\et_pessoa\et_pessoa_body.sql
-@@ packages\et_relatorio\et_relatorio_body.sql
-@@ packages\et_tratamento\et_tratamento_body.sql
+@ packages\et_cirurgia\et_cirurgia_body.sql
+@ packages\et_consulta\et_consulta_body.sql
+@ packages\et_pessoa\et_pessoa_body.sql
+@ packages\et_relatorio\et_relatorio_body.sql
+@ packages\et_tratamento\et_tratamento_body.sql
 
-@@ CBD_triggers.sql
+@ CBD_triggers.sql
 
-@@ dummy_data\index.sql
+@ dummy_data\index.sql
