f92fbd5e98180cff52adad781664b940b8c463fa
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-04-15 16:13:59 +0100

    commit

--- CBD_main.sql
+++ CBD_main.sql
@@ -2,24 +2,25 @@
 
 @@ CBD_eliminar.sql
 
-@@ ../packages/exception_handler/exception_handler.sql
-@@ ../packages/exception_handler/exception_handler_body.sql
-
-@@ ../packages/et_cirurgia/et_cirurgia.sql
-@@ ../packages/et_consulta/et_consulta.sql
-@@ ../packages/et_pessoa/et_pessoa.sql
-@@ ../packages/et_relatorio/et_relatorio.sql
-@@ ../packages/et_tratamento/et_tratamento.sql
-
 @@ CBD_tabelas.sql
+
+@@ ..\packages\exception_handler\exception_handler.sql
+@@ ..\packages\exception_handler\exception_handler_body.sql
+
+@@ ..\packages\et_cirurgia\et_cirurgia.sql
+@@ ..\packages\et_consulta\et_consulta.sql
+@@ ..\packages\et_pessoa\et_pessoa.sql
+@@ ..\packages\et_relatorio\et_relatorio.sql
+@@ ..\packages\et_tratamento\et_tratamento.sql
+
 @@ CBD_views.sql
 
-@@ ../packages/et_cirurgia/et_cirurgia_body.sql
-@@ ../packages/et_consulta/et_consulta_body.sql
-@@ ../packages/et_pessoa/et_pessoa_body.sql
-@@ ../packages/et_relatorio/et_relatorio_body.sql
-@@ ../packages/et_tratamento/et_tratamento_body.sql
+@@ ..\packages\et_cirurgia\et_cirurgia_body.sql
+@@ ..\packages\et_consulta\et_consulta_body.sql
+@@ ..\packages\et_pessoa\et_pessoa_body.sql
+@@ ..\packages\et_relatorio\et_relatorio_body.sql
+@@ ..\packages\et_tratamento\et_tratamento_body.sql
 
 @@ CBD_triggers.sql
 
-@@ dummy_data/index.sql
+@@ dummy_data\index.sql
