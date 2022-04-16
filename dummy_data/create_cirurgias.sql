41ed3461e51c6d87cdc4802802ed21021e556f5b
Afonso Santos <afonso.faisca1@gmail.com>
2022-04-16 22:12:27 +0100

    Update cirurgias dummy

--- dummy_data/create_cirurgias.sql
+++ dummy_data/create_cirurgias.sql
@@ -1,19 +1,18 @@
-DECLARE
-
-CURSOR cur_tipo_cirurgias IS
-	SELECT id_tipo_cirurgia FROM tipo_cirurgia;
-
 BEGIN
 
-		FOR rec_tipo_cirurgia IN cur_tipo_cirurgias
-		LOOP
-			et_cirurgia.registar_cirurgia(
-					p_id_tratamento => 1,
-					p_id_tipo_cirurgia => rec_tipo_cirurgia.id_tipo_cirurgia,
-					p_relatorio => 'Cirurgia OK.',
-					p_t_nif => t_nif(123123006, 123123007)
-			);
-		END LOOP;
+    et_cirurgia.registar_cirurgia(
+            p_id_tratamento => 1,
+            p_id_tipo_cirurgia => 18,
+            p_relatorio => 'Cirurgia OK.',
+            p_t_nif => et_pessoa.t_nif(123123006)
+    );
+    
+    et_cirurgia.registar_cirurgia(
+            p_id_tratamento => 2,
+            p_id_tipo_cirurgia => 6,
+            p_relatorio => 'Cirurgia OK.',
+            p_t_nif => et_pessoa.t_nif(123123007)
+    );
 
 END;
 /
