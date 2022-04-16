b7e2b23660d2f340b8f2e7c1fbcccb867713258e
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-04-16 22:04:17 +0100

    fé

--- dummy_data/create_user_exceptions.sql
+++ dummy_data/create_user_exceptions.sql
@@ -5,88 +5,77 @@ BEGIN
     exception_handler.add_user_exception(
             -20001,
             'exception_not_defined',
-            'Exceção não definida.'
-        );
+            'Exceção não definida.');
 
     exception_handler.add_user_exception(
             -20002,
             'system_exception',
-            'Exceção de sistema ou genérica acionada no "EXCEPTION WHEN OTHERS THEN ..."'
-        );
+            'Exceção de sistema ou genérica acionada no "EXCEPTION WHEN OTHERS THEN ..."');
 
     exception_handler.add_user_exception(
             -20799,
             'consulta_em_tratamento_finalizado',
-            'Tentativa de registo de consulta em tratamento já finalizado.'
-        );
+            'Tentativa de registo de consulta em tratamento já finalizado.');
 
     exception_handler.add_user_exception(
             -20798,
             'alteracao_consulta',
-            'Tentativa de alteração de consulta.'
-        );
+            'Tentativa de alteração de consulta.');
 
     exception_handler.add_user_exception(
             -20899,
             'menor_de_idade',
-            'Não é permitido o registo de pessoas com menos de 18 anos.'
-        );
+            'Não é permitido o registo de pessoas com menos de 18 anos.' );
 
     exception_handler.add_user_exception(
             -20888,
             'paciente_sem_tratamento',
-            'Paciente sem tratamento registado.'
-        );
+            'Paciente sem tratamento registado.');
 
     exception_handler.add_user_exception(
             -20887,
             'nome_invalido',
-            'Nome da pessoa inválido. O nome não pode conter números ou carateres especiais'
-        );
+            'Nome da pessoa inválido. O nome não pode conter números ou carateres especiais' );
 
     exception_handler.add_user_exception(
             -20699,
             'alteracao_relatorio',
-            'Tentativa de alteração de um relatório.'
-        );
+            'Tentativa de alteração de um relatório.');
 
     exception_handler.add_user_exception(
             -20998,
             'tratamento_repetido',
-            'Tentativa de abertura de tratamento repetido para o mesmo paciente.'
-        );
+            'Tentativa de abertura de tratamento repetido para o mesmo paciente.');
 
     exception_handler.add_user_exception(
             -20997,
             'tratamento_nao_encontrado',
-            'Tratamento não encontrado.'
-        );
+            'Tratamento não encontrado.');
 
     exception_handler.add_user_exception(
             -20996,
             'finalizacao_repetida',
-            'Tentativa de finalizar tratamento já finalizado.'
-        );
+            'Tentativa de finalizar tratamento já finalizado.');
 
     exception_handler.add_user_exception(
             -20995,
             'alteracao_invalida',
-            'Tentativa de alteração do tratamento não permitida.'
-        );
+            'Tentativa de alteração do tratamento não permitida.');
 
     exception_handler.add_user_exception(
             -20101,
             'excecao_mal_formatada',
-            'Nome/código de exceção mal formatada.'
-        );
+            'Nome/código de exceção mal formatada.');
 
-    exception_handler.add_user_exception(-20500, 'area_atuacao_nao_corresponde', 'Área de atuação não corresponde!');
+    exception_handler.add_user_exception(
+            -20500,
+            'area_atuacao_nao_corresponde',
+            'Área de atuação não corresponde!');
 
     exception_handler.add_user_exception(
             -20501,
             'alteracao_cirurgia',
-            'Alteração/remoção de uma cirurgia não permitida!'
-        );
+            'Alteração/remoção de uma cirurgia não permitida!');
 
 END;
 /
