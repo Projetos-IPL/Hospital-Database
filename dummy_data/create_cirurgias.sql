37c351c786224c3b778f1aea14242ed5f441522b
Afonso Santos <afonso.faisca1@gmail.com>
2022-04-17 12:08:30 +0100

    Fix dummy data

--- dummy_data/create_cirurgias.sql
+++ dummy_data/create_cirurgias.sql
@@ -4,14 +4,14 @@ BEGIN
             p_id_tratamento => 1,
             p_id_tipo_cirurgia => 18,
             p_relatorio => 'Cirurgia OK.',
-            p_t_nif => et_pessoa.t_nif(123123006)
+            p_t_nif => et_pessoa.t_nif(123123007)
     );
     
     et_cirurgia.registar_cirurgia(
             p_id_tratamento => 2,
             p_id_tipo_cirurgia => 6,
             p_relatorio => 'Cirurgia OK.',
-            p_t_nif => et_pessoa.t_nif(123123007)
+            p_t_nif => et_pessoa.t_nif(123123006)
     );
 
 END;
