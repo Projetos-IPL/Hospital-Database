88bc86b6d653168aeb44f2e4cd94b1f56cb8ce60
Afonso Santos <afonso.faisca1@gmail.com>
2022-05-09 19:21:46 +0100

    commit

--- CBD_views.sql
+++ CBD_views.sql
@@ -4,15 +4,18 @@ SELECT  p.nif,
         p.ult_nome,
         p.dta_nasc,
         t.id_processo,
-       aa.id_area_atuacao,
+        aa.id_area_atuacao,
         aa.descricao AS "Descricao Area Atuacao",
         t.dta_inicio,
-        t.dta_alta
-FROM processo       t,
+        t.dta_alta,
+        ep.descricao
+FROM processo         t,
      area_atuacao     aa,
-     pessoa           p
+     pessoa           p,
+     estado_paciente  ep
 WHERE t.nif = p.nif
-  AND t.id_area_atuacao = aa.id_area_atuacao;
+  AND t.id_area_atuacao = aa.id_area_atuacao
+  AND t.id_estado_paciente = ep.id_estado_paciente;
 /
 
 CREATE OR REPLACE VIEW medico_area_atuacao_view AS
@@ -41,6 +44,27 @@ FROM   pessoa pe,
        telefone te
 WHERE  pe.nif = pa.nif
        AND pe.nif = te.nif;
+/
+
+CREATE OR REPLACE VIEW processo_andamento_dados_view AS
+SELECT  p.nif,
+        p.prim_nome,
+        p.ult_nome,
+        p.dta_nasc,
+        t.id_processo,
+        aa.id_area_atuacao,
+        aa.descricao AS "Descricao Area Atuacao",
+        t.dta_inicio,
+        t.dta_alta,
+        ep.descricao
+FROM processo         t,
+     area_atuacao     aa,
+     pessoa           p,
+     estado_paciente  ep
+WHERE t.nif = p.nif
+  AND t.id_area_atuacao = aa.id_area_atuacao
+  AND t.id_estado_paciente = ep.id_estado_paciente
+  AND t.dta_alta IS NULL;
 /
 
 
