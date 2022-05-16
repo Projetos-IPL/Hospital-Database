ad0874a77900c61adeeb3d0f224808253ad8f4c5
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-05-16 19:26:37 +0100

    adicionados tablespaces

--- CBD_tablespaces.sql
+++ CBD_tablespaces.sql
@@ -1,6 +1,6 @@
 -- Projeto CBD - 2021/2022
 -- Grupo 5 (Afonso Santos - 2210640, Iúri Raimundo - 2210651)
--- Descrição: Table spaces
+-- Descrição: Tablespaces
 
 CREATE TABLESPACE tbs_tables
     DATAFILE 'tbs_tables.dbf'
@@ -9,5 +9,3 @@ CREATE TABLESPACE tbs_tables
 CREATE TABLESPACE tbs_indexes
     DATAFILE 'tbs_indexes.dbf'
     SIZE 1024M;
-
-SELECT * FROM dba_data_files;
