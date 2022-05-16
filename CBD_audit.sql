0c4b6146fefce874ecca317d8f0ea1ae1a6b2218
Afonso Santos <afonso.faisca1@gmail.com>
2022-05-16 17:24:18 +0100

    Audit

---
+++ CBD_audit.sql
@@ -0,0 +1,9 @@
+-- Projeto CBD - 2021/2022
+-- Grupo 5 (Afonso Santos - 2210640, Iúri Raimundo - 2210651)
+-- Descrição: script para configuração de auditoria.
+
+
+AUDIT
+	UPDATE
+ON PROJETO.PROCESSO
+WHENEVER NOT SUCCESSFUL;
