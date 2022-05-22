697c13518e05d427c931bc942282ae9ae6048415
Afonso Santos <afonso.faisca1@gmail.com>
2022-05-22 12:00:02 +0100

    Remove delete script

--- CBD_eliminar.sql
+++
@@ -1,8 +0,0 @@
--- Projeto CBD - 2021/2022
--- Grupo 5 (Afonso Santos - 2210640, Iúri Raimundo - 2210651)
--- Descrição: script para apagar objetos da base de dados
-
-
-DROP USER PROJETO CASCADE;
-
-COMMIT;
