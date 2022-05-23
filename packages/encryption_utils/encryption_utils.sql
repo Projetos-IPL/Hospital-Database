fe89748a1ea1e23e55e4acc0e45fbb7fc74ced47
Afonso Santos <afonso.faisca1@gmail.com>
2022-05-23 18:44:21 +0100

    added docs

--- packages/encryption_utils/encryption_utils.sql
+++ packages/encryption_utils/encryption_utils.sql
@@ -5,8 +5,10 @@
 
 CREATE OR REPLACE PACKAGE encryption_utils AS
 
+	-- Cifra uma string, retornando um objeto do tipo RAW.
 	FUNCTION encrypt_str (p_str IN VARCHAR2) RETURN RAW DETERMINISTIC;
 
+	-- Decifra uma string cifrada, retornando um VARCHAR.
 	FUNCTION decrypt_str (p_str IN VARCHAR2) RETURN VARCHAR2 DETERMINISTIC;
 
 END encryption_utils;
