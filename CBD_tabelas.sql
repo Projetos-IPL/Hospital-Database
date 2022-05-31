adfe24db28c6c71a3d19c37f3b160e8f68b939d7
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-05-31 18:17:25 +0100

    adicionados tablespaces

--- CBD_tabelas.sql
+++ CBD_tabelas.sql
@@ -206,3 +206,4 @@ CREATE TABLE PROJETO.encryption_key (
 		key VARCHAR2(200),
 		CONSTRAINT pk_encryption_key PRIMARY KEY (key)
 ) TABLESPACE tbs_tables;
+
--- CBD_views.sql
+++ CBD_views.sql
@@ -160,3 +160,6 @@ AS
 SELECT *
 FROM dba_audit_object
 WHERE obj_name = 'PROCESSO';
+
+
+
